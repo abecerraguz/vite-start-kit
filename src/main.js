26f3d5e5035cb066d2d8e28cb5e0d15c58b46706
abecerraguz <alejandro.becerra@publimetro.cl>
2023-05-11 15:16:49 -0400

    Se ajustan los logotipos

--- src/main.js
+++ src/main.js
@@ -5,13 +5,11 @@ import { setupCounter } from './components/counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
+
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Javascript!</h1>
+    <h1>Hello Github!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
